3ec58b62324c310ada2eac42579cd67b04831158
Roberts Rubenis <Roberts Rubenis>
2017-05-02 01:06:08 +0300

    Otrais commit

---
+++ Assets/Scripts/MainMenuPogas.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuPogas : MonoBehaviour {
+
+    public void SaktPoga(string newLevel)
+    {
+        SceneManager.LoadScene(newLevel);
+    }
+
+    public void IzietPoga()
+    {
+        Application.Quit();
+    }
+}
